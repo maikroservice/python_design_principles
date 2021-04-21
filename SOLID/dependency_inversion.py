2a484a07bf773fcedffbcac6666ff77995e9e6d7
Maik Rosenheinrich <lefthand3r@gmail.com>
2021-04-21 07:52:10 +0200

    updated DIP

--- SOLID/dependency_inversion.py
+++ SOLID/dependency_inversion.py
@@ -29,7 +29,7 @@ class Relationships:
 
 # high level module -> Research
 class Research:
-    def __init__(self, relationships):
+    def __init__(self, relationships): # one issue with the code is that it relies on the implementation of relations
         relations = relationships.relations
         for r in relations:
             if r[0].name == 'John' and r[1] == Relationship.PARENT:
@@ -45,3 +45,47 @@ relationships.add_parent_and_child(parent, child1)
 relationships.add_parent_and_child(parent, child2)
 
 Research(relationships)
+
+"""
+In order to abide to the Dependency Inversion Principle we should define a visible interface (RelationshipLookup) 
+that will lead to the lookup being part of the Relationship-class and thus freeing the developer from the need to know
+how the lookup was implemented because they can depend on the interface.
+"""
+
+class RelationshipLookup:
+    @abstractmethod
+    def find_all_children_of(self, parent):
+        pass
+
+class ImprovedRelationships(RelationshipLookup):
+    def __init__(self):
+        self.relations = []
+
+    def add_parent_and_child(self, parent, child):
+        self.relations.append(
+            (parent, Relationship.PARENT, child)
+        )
+        self.relations.append(
+            (child, Relationship.CHILD, parent)
+        )
+
+    def find_all_children_of(self, parent):
+        for r in self.relations:
+            if r[0].name == parent and r[1] == Relationship.PARENT:
+                yield r[2]
+
+class ImprovedResearch:
+    def __init__(self, lookup):
+        for child in lookup.find_all_children_of("John"):
+            print(f"John has a child called {child}")
+
+            
+parent = Person("John")
+child1 = Person("Chris")
+child2 = Person("Matt")
+
+relationships = ImprovedRelationships()
+relationships.add_parent_and_child(parent, child1)
+relationships.add_parent_and_child(parent, child2)
+
+ImprovedResearch(relationships)
